40d4864f054e0e12fc52e0804c2968a6d2f006e1
rcherp89 <robincherpitel@gmail.com>
2017-05-13 21:09:48 +0200

    zefzefez

--- Arbre.java
+++ Arbre.java
@@ -2,18 +2,18 @@ import java.util.*;
 
 class Arbre {
 	
-	private String debut, fin;
 	private List<Arbre> elements;
+	private String debut, fin;
 
-	public Arbre(String debut, String fin, List<Arbre> elements){
-		this.debut = debut;
-		this.fin = fin;
-		this.elements = elements;
-	}
-	
 	public Arbre(String debut, List<Arbre> elements){
 		this.debut = debut;
 		this.fin = "";
+		this.elements = elements;
+	}
+	
+	public Arbre(String debut, String fin, List<Arbre> elements){
+		this.debut = debut;
+		this.fin = fin;
 		this.elements = elements;
 	}
 	
